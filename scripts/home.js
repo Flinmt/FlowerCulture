de7d32d4725b600a89e5b416ccf4b52161217639
Vitor José <vitorlimagerr.profissional@gmail.com>
2023-06-01 15:27:31 -0300

    home page conclusion

--- scripts/home.js
+++ scripts/home.js
@@ -27,3 +27,30 @@ function basketShowHide() {
         hide = true;
     }
 }
+
+// More Products
+
+const esperar = async (miliseconds) => {
+    await new Promise(resolve => {
+        return setTimeout(resolve, miliseconds)
+    });
+}
+
+var show = false;
+
+async function productsShowHide() {
+    if (show == false) {
+        document.getElementById("more").style.display = "block";
+        document.getElementById("all").innerHTML = "Hide products";
+        await esperar(200);
+        document.getElementById("more").style.transform = "translateX(0%)";
+        show = true;
+    } else {
+        document.getElementById("more").style.transform = "translateX(-110%)";
+        await esperar(800);
+        document.getElementById("more").style.display = "none";
+        document.getElementById("all").innerHTML = "View all products";
+        show = false;
+    }
+}
+
